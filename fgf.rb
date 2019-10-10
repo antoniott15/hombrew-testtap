1c7c90905b1e78797d163ad369c4faf5a8d815c9
Antonio <antonio.toche@utec.edu.pe>
2019-10-10 16:35:12 -0500

    fg

--- fgf.rb
+++ fgf.rb
@@ -1,14 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Fgf < Formula
   desc "CLI for google fonts installation in flutter"
   homepage ""
-  url "https://github.com/antoniott15/fgf/archive/0.0.1.tar.gz"
-  sha256 "ecc225e84a60eecf76a70ab6d7f516c2e8a4dc7a5a0f07bd693791399d61ec38"
+  url "https://github.com/antoniott15/fgf/archive/v0.0.1.tar.gz"
+  sha256 "4df7a29948a3d33e1b6f50329ba73f3a9a2e4c391dbe66fb3011a172fcd131c2"
 
+  # depends_on "cmake" => :build
 
   def install
-    system "install go.mod","go build && ./fgf"
-
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
   end
 
-
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test fgf`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
