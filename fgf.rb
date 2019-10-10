0dc826692760996a955ca1a244434438ff8e717a
Antonio <antonio.toche@utec.edu.pe>
2019-10-10 00:39:13 -0500

    add formula

---
+++ fgf.rb
@@ -0,0 +1,11 @@
+class Fgf < Formula
+  desc "CLI for google fonts installation in flutter"
+  homepage ""
+  url "https://github.com/antoniott15/fgf/archive/v1.0.0.tar.gz"
+  sha256 "997448db6a4d5c9a14ab2930807efd37a5188b44ca7143ebd34cdac696af684f"
+
+  def install
+    bin.install "fgf"
+  end
+
+end
