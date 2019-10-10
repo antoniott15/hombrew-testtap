131be6a1395b764dc0f2251b74d8981b79212fe4
Antonio <antonio.toche@utec.edu.pe>
2019-10-10 15:58:29 -0500

    deletes

--- fgf.rb
+++ fgf.rb
@@ -1,15 +1,15 @@
-require "language/go"
+# require "language/go"
 
-class Fgf < Formula
-  desc "CLI for google fonts installation in flutter"
-  homepage ""
-  url "https://github.com/antoniott15/fgf/archive/v1.0.0.tar.gz"
-  version "0.0.1"
-  sha256 "997448db6a4d5c9a14ab2930807efd37a5188b44ca7143ebd34cdac696af684f"
+# class Fgf < Formula
+#   desc "CLI for google fonts installation in flutter"
+#   homepage ""
+#   url "https://github.com/antoniott15/fgf/archive/v1.0.0.tar.gz"
+#   version "0.0.1"
+#   sha256 "997448db6a4d5c9a14ab2930807efd37a5188b44ca7143ebd34cdac696af684f"
 
 
-  def install
-    bin.install "go.mod"
-  end
+#   def install
+#     bin.install "go.mod"
+#   end
 
-end
+# end
